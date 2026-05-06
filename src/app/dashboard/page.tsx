eda74ac397ee9ccfc29801c031b3bc25c9445477
g0dlessjs <juangalarce.dev@gmail.com>
2026-05-06 10:11:12 -0400

    feat(auth): protect dashboard and display user session info
    - Create auth helper in src/helpers/auth.ts using NextAuth.
    
    - Implement server-side session check in DashboardPage.
    
    - Add redirect to signin if session is not present.
    
    - Display user name and email in a WidgetItem.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,25 @@
 import { WidgetItem } from "@/components";
+import { auth } from "@/helpers/auth";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {/* <WidgetItem/> */}
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
+      <WidgetItem title="Usuario desde Server Side">
+        <div className="flex flex-col items-center">
+          <p className="font-bold">{session.user?.name}</p>
+          <p className="text-slate-300">{session.user?.email}</p>
+        </div>
+        <button className="bg-red-500 text-white px-4 py-2 rounded-2xl hover:cursor-pointer">
+          Logout
+        </button>
+      </WidgetItem>
     </div>
   );
 }
