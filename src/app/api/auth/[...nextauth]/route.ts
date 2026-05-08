8ae7ab0f313e0ab83ef33e8b740253ed10c231c3
g0dlessjs <juangalarce.dev@gmail.com>
2026-05-08 09:30:57 -0400

    feat(auth): integrate PrismaAdapter and AuthJS models
    - Add Account, Session, User, and VerificationToken models to Prisma schema.
    
    - Install and configure @auth/prisma-adapter in NextAuth route.
    
    - Set authentication strategy to JWT.
    
    - Add redirect to dashboard in root page.
    
    - Update dependencies and generate auth migration.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/lib/prisma";
 
 export const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -13,6 +18,10 @@ export const authOptions: NextAuthConfig = {
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const { handlers } = NextAuth(authOptions);
